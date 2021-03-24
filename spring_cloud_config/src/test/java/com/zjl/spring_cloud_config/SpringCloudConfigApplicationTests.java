4ca18b8821061755180c2ce0b0e0cf67f858636a
13588330822 <13588330822@163.com>
2021-03-24 10:23:25 +0800

    配置中心

---
+++ spring_cloud_config/src/test/java/com/zjl/spring_cloud_config/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjl.spring_cloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
