57694681ffef4fae5855999792a179ff40a49b3a
13588330822@163.com <13588330822@163.com>
2021-07-01 13:31:50 +0800

    openfegin调通

--- consumer/src/main/java/com/zjl/consumer/service/ProducerService.java
+++ consumer/src/main/java/com/zjl/consumer/service/ProducerService.java
@@ -1,10 +1,9 @@
 package com.zjl.consumer.service;
 
-import com.zjl.commons.util.WebResponse;
+import com.zjl.commons.util.response.WebResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -12,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Date: 2021-06-23 23:57
  * @Description:
  */
-@FeignClient(value = "producer",url = "localhost:8084/producer/")
+@Component
+@FeignClient(value = "producer-service",path = "/producer")
 public interface ProducerService {
 
     @GetMapping(value = "/api/data")
